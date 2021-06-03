51d16cdeba6e8fa17559449b125c6b36d8ba332b
Everton Otavio Afonso <evertonafonso0@gmail.com>
2021-06-02 22:26:05 -0300

    Header da aplicação

---
+++ src/components/Logo/index.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+import "./styles.css";
+
+export default function Logo() {
+    return(
+        <h1 className="logo"> 
+            <span>LAST </span>
+            NOTES
+        </h1>
+    );
+}
---
+++ src/components/NotesArea/index.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+import "./styles.css";
+
+export default function NotesArea() {
+    return (
+        <div>
+            <h1>Contatos</h1>
+        </div>
+    );
+}
